087f2a3f6056d4135cc838ea7142a09558b03f42
danielnanuk <378353195@qq.com>
2016-12-22 11:06:20 +0800

    修改ubuntu apt源为阿里镜像源
    修改tomcat，增加webapps挂载

--- tomcat8/docker.sh
+++ tomcat8/docker.sh
@@ -10,7 +10,7 @@ build() {
   docker build . -t 'nielcho/tomcat8:v1'
 }
 run() {
-  LCID=`docker run -itdP 'nielcho/tomcat8:v1'`
+  LCID=`docker run -v $PWD/webapps:/opt/apache-tomcat-8.5.4/webapps -itdP 'nielcho/tomcat8:v1'`
   CID=${LCID:0:12}
   echo "tomcat        : http://`docker port $CID 8080`"
   echo "supervisorctl : docker exec -it $CID supervisorctl"
---
+++ tomcat8/webapps/ROOT/index.jsp
@@ -0,0 +1,1 @@
+tomcat-8.5.4 in docker container
